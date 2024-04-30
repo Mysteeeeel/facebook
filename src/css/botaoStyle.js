d8728a11439a95aab991e787a4edf701c45c9753
Lucas Prestes <lucasprestes0606@gmail.com>
2024-04-30 16:30:17 -0300

    projetofacebook

---
+++ src/css/botaoStyle.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    button: {
+     paddingVertical: 20,
+     paddingHorizontal: 40,
+     borderColor: '#EBECEE',
+     borderWidth: 1,
+     borderRadius: 5,
+     marginVertical: 10,
+    },
+    buttonPressed: {
+        backgroundColor: '#0056b3'
+    },
+    buttonText: {
+        color: '#000000',
+        fontSize: 16,
+        textAlign: 'center',
+    }
+  });
+  
